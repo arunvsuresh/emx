b5216d82d72b3630f7dd893ef3a1cde6861af2ff
Ubuntu <ubuntu@ip-172-31-47-254.us-west-2.compute.internal>
2018-09-12 22:06:00 +0000

    add responses to request queries

--- emx.py
+++ emx.py
@@ -1,9 +1,46 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def emx():
-    return "Hello World!"
+
+    if request.args.get("q") == "Email Address":
+	    return "arunvsuresh@gmail.com"
+
+    if request.args.get("q") == "Name":
+        return "Arun Suresh"
+
+    if request.args.get("q") == "Phone":
+	return "4088216532"
+
+    if request.args.get("q") == "Position":
+	return "Software Engineer"
+
+    if request.args.get("q") == "Years":
+	return "3"
+
+    if request.args.get("q") == "Referrer":
+        return "LinkedIn"
+
+    if request.args.get("q") == "Degree":
+        return "University of California, Riverside - Bachelors in Science, Business Economics"
+
+    if request.args.get("q") == "Resume":
+        return "https://docs.google.com/document/d/1cQtQ-w3J_UbXQU_49kQ9XmkR9sGuN1OmhU-h7dX3jXE/edit"
+
+    if request.args.get("q") == "Source":
+	return "https://github.com/arunvsuresh/emx"
+
+    if request.args.get("q") == "Status":
+        return "Yes"
+
+    if request.args.get("q") == "Puzzle":
+	request.args.get("q"), request.args.get("d")
+    return "OK"
+
+@app.route("/fullname")
+def fullname():
+    return "Arun Suresh"
 
 if __name__=='__main__':
     app.run(host='0.0.0.0')
