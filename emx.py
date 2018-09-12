740c0ea52f2891153b1460d5f3509f9d26928d18
Arun Suresh <arunsuresh@aruns-macbook.home>
2018-09-12 16:28:06 -0400

    rename files

--- emx.py
+++ emx.py
@@ -2,8 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def emx_webservice():
+def emx():
     return "Hello World!"
 
 if __name__=='__main__':
     app.run(host='0.0.0.0')
+
